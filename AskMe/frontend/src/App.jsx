f3be64bd6b173636ff5e2e121e52c162993e645f
prustyprasanna <prustyprasanna95@gmail.com>
2025-07-12 14:05:49 +0530

    buttons done

--- AskMe/frontend/src/App.jsx
+++ AskMe/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AskQuestion from "./AskQuestion";
 
+
 function App() {
   return (
     <>
-      <h1>Welcome to AskMe!</h1>
+      
       <AskQuestion />
+      
     </>
   );
 }
