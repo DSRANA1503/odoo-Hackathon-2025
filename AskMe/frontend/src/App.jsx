6e7a8adcdec46df03c497860237b2e3bc703352b
prustyprasanna <prustyprasanna95@gmail.com>
2025-07-12 14:30:18 +0530

    navigation

--- AskMe/frontend/src/App.jsx
+++ AskMe/frontend/src/App.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import AskQuestion from "./AskQuestion";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import AskQuestion from './AskQuestion';
 import LoginPage from './LoginPage';
-import HomePage from './homepage';
-
 
 function App() {
   return (
-    <>
-<<<<<<< HEAD
-      
-      <AskQuestion />
-      
-=======
-      <h1>Welcome to AskMe!</h1>
-      <HomePage/>
-      <LoginPage/>
-      <AskQuestion />
-      
-      
->>>>>>> 32ef249d9a8b2168ef8c2df706bec01c0e451978
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/ask" element={<AskQuestion />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/question/:id" element={<div>Question Detail Page</div>} />
+        <Route path="/question/:id/answer" element={<div>Add Answer Page</div>} />
+      </Routes>
+    </Router>
   );
 }
 
